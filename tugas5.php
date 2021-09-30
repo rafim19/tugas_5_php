de9bdbdb6276505f3206b03b0aea61452dc9efcb
Rafi Muhammad <rafi.muhammad001@binus.ac.id>
2021-09-30 21:00:01 +0700

    destructuring

--- tugas5.php
+++ tugas5.php
@@ -10,43 +10,99 @@
   $row4OR = true or true;
 
   echo "Tabel Logika 1111<br>";
-  echo ("
-    <table>
-      <tr>
-        <thead>
-          <th>Input 1</th>
-          <th>Input 2</th>
-          <th>AND</th>
-          <th>OR</th>
-        </thead>
-      </tr>
-      <tr>
-        <td>false</td>
-        <td>false</td>
-        <td>$row1AND</td>
-        <td>$row1OR</td>
-      </tr>
-      <tr>
-        <td>false</td>
-        <td>true</td>
-        <td>$row2AND</td>
-        <td>$row2OR</td>
-      </tr>
-      <tr>
-        <td>true</td>
-        <td>false</td>
-        <td>$row3AND</td>
-        <td>$row3OR</td>
-      </tr>
-      <tr>
-        <td>true</td>
-        <td>true</td>
-        <td>$row4AND</td>
-        <td>$row4OR</td>
-      </tr>
-    </table>
-  ");
+?>
+<table border="1">
+  <tr>
+    <thead>
+      <th>Input 1</th>
+      <th>Input 2</th>
+      <th>AND</th>
+      <th>OR</th>
+    </thead>
+  </tr>
+  <tr>
+    <td>false</td>
+    <td>false</td>
+    <td><?php 
+        if ($row1AND == false) {
+          echo 0;
+        } else {
+          echo 1;
+        }
+      ?>
+    </td>
+    <td><?php 
+        if ($row1OR == false) {
+          echo 0;
+        } else {
+          echo 1;
+        }
+      ?>
+    </td>
+  </tr>
+  <tr>
+    <td>false</td>
+    <td>true</td>
+    <td><?php 
+        if ($row2AND == false) {
+          echo 0;
+        } else {
+          echo 1;
+        }
+      ?>
+    </td>
+    <td><?php 
+        if ($row2OR == false) {
+          echo 0;
+        } else {
+          echo 1;
+        }
+      ?>
+    </td>
+  </tr>
+  <tr>
+    <td>true</td>
+    <td>false</td>
+    <td><?php 
+        if ($row3AND == false) {
+          echo 0;
+        } else {
+          echo 1;
+        }
+      ?>
+    </td>
+    <td><?php 
+        if ($row3OR == false) {
+          echo 0;
+        } else {
+          echo 1;
+        }
+      ?>
+    </td>
+  </tr>
+  <tr>
+    <td>true</td>
+    <td>true</td>
+    <td><?php 
+        if ($row4AND == false) {
+          echo 0;
+        } else {
+          echo 1;
+        }
+      ?>
+    </td>
+    <td><?php 
+        if ($row4OR == false) {
+          echo 0;
+        } else {
+          echo 1;
+        }
+      ?>
+    </td>
+  </tr>
+</table>
 
+<?php
   // Nomor 2
   $input1_1 = "Ayo";
   $input1_2 = "Bersama";
@@ -54,26 +110,26 @@
   $input2_2 = "Niomic";
 
   echo "<h1>Operator String</h1>";
-  echo ("
-    <table>
-      <tr>
-        <thead>
-          <th>Input 1</th>
-          <th>Input 2</th>
-          <th>Hasil</th>
-        </thead>
-      </tr>
-      <tr>
-        <td>$input1_1</td>
-        <td>$input2_1</td>
-        <td>$input1_1 "."$input2_1</td>
-      </tr>
-      <tr>
-        <td>$input2_1</td>
-        <td>$input2_2</td>
-        <td>$input2_1 "."$input2_2</td>
-      </tr>
-    </table>
-  ");
+?>
+<table border="1">
+  <tr>
+    <thead>
+      <th>Input 1</th>
+      <th>Input 2</th>
+      <th>Hasil</th>
+    </thead>
+  </tr>
+  <tr>
+    <td><?php echo $input1_1 ?></td>
+    <td><?php echo $input2_1 ?></td>
+    <td><?php echo "$input1_1 " . $input2_1 ?></td>
+  </tr>
+  <tr>
+    <td><?php echo $input2_1 ?></td>
+    <td><?php echo $input2_2 ?></td>
+    <td><?php echo "$input2_1 " . $input2_2 ?></td>
+  </tr>
+</table>
+<?php
   echo "Gabungan : $input1_1 "."$input2_1 "."$input1_2 "."$input2_2<br>";
 ?>
